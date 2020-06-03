8317128dcd63c938724998f36cf978f3ad75ff0c
leinlin <345036769@qq.com>
2020-06-04 04:12:46 +0800

    修改点规则

--- grabber.py
+++ grabber.py
@@ -1,65 +1,77 @@
 #!/usr/bin/python
-#coding:utf-8
-import os # path manipulation
+# coding:utf-8
+import json
+import os  # path manipulation
 import urllib as urllib
+
 import requests
+from xmltodict import parse
 
 status = 'not done yet'
+danbooru_folder = os.getcwd() + '/Pictures/'
 
-# change this to your danbooru folder
-# it might look something like this: '/users/YourUserName/DanbooruPics'
-# make sure the folder already exists!
-danbooru_folder = '/Users/chaoguo/Pictures/'
 
-# generate tag argument to be used in url and folder creation
-def generate_tag_argv(tagList):
-	tag_argv = ''
-	for tag in tagList:
-		tag_argv = tag_argv + tag + '+'
-	tag_argv = tag_argv[:-1]
+def file_extension(path):
+    return os.path.splitext(path)[1]
 
-	return tag_argv
+
+def download(url, path):
+    try:
+        request = requests.get(url, timeout=30)
+
+        with open(path, "wb") as f:
+            f.write(request.content)
+    except requests.exceptions.RequestException as e:
+        print(e)
+
 
 # request json, get urls of pictures and download them
-def grabber(tag_argv,page_num):
-	r = requests.get('https://danbooru.donmai.us/posts.json?tags='+tag_argv+'&page='+str(page_num))
-	streams = r.json()
-	# check if all pages have been visited
-	if len(streams) == 0:
-		print("All pictures have been downloaded!")
-		global status
-		status = 'done'
-	else:
-		# check if directory already exists
-		if (os.path.exists(danbooru_folder+tag_argv) == False):
-			os.mkdir(danbooru_folder+tag_argv)
+def grabber(query_str):
+    r = requests.get('https://danbooru.me/post/index.xml?' + query_str)
+    xml = parse(r.text, r.apparent_encoding)
 
-		url = []
-		for post in streams:
-			if 'file_url' in post:
-				url.append(post['file_url'])
-		target = ['https://danbooru.donmai.us'+x for x in url]
+    streams = xml["posts"]["post"]
+    # check if all pages have been visited
+    if len(streams) == 0:
+        print("All pictures have been downloaded!")
+        global status
+        status = 'done'
+    else:
+        # check if directory already exists
+        if not os.path.exists(danbooru_folder):
+            os.mkdir(danbooru_folder)
 
-		# download
-		for address in target:
-			urllib.urlretrieve(address,danbooru_folder+tag_argv+'/'+address.split('/')[-1])
+        url = []
+        for post in streams:
+            if ('@file_url' in post) and ('@score' in post):
+                extension = file_extension(post['@file_url'])
+                if (post['@score'] >= 20) and (extension == ".jpg" or extension == ".png"):
+                    url.append(post)
+
+        # download
+        for post in url:
+            extension = file_extension(post['@file_url'])
+            fileName = str(post['@id']) + extension
+
+            url = "https://danbooru.me/data/" + post['@file_url'].split('/')[-1]
+            path = danbooru_folder + '/' + fileName
+            print("Download file:" + url)
+            download(url, path)
+            #urllib.urlretrieve(url, path)
 
 
 def main():
-	page_num = input('Enter the number of pages you want to download. To download all, simply enter a super large number:')
-	taginput = raw_input('Enter tags,separated by space:') 
+    queryStr = "tags=azur_lane+-rating:safe+order:score&limit=1000"  # 'sys.argv[1]
 
-	n = 1
-	while n <= page_num and status == 'not done yet':
-		tagList = taginput.split(' ')
-		tag_argv = generate_tag_argv(tagList)
-		grabber(tag_argv,n)
-		n = n + 1
+    n = 1
+    while n <= 1000 and status == 'not done yet':
+        query = queryStr + "&page=" + str(n)
+        print(query)
+        grabber(query)
+        n = n + 1
 
-	print('Download successful!')
-	u2 = u'どうぞ、召し上がってください！'
-	print u2
+    print('Download successful!')
 
 
 if __name__ == '__main__':
-	main()
+    main()
